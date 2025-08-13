46ba1a910163d4d650e4eadc22940ec319a06db0
Naveen G <gangineninaveen132@gmail.com>
2025-08-13 17:46:51 +0530

    conversation b/w Modi and Putin

---
+++ 2-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Modi : Hello Putin Bhai, how are you"
+echo "Putin : I am super good, how are you my friend"
+echo "Modi : great my friend, lets make a meeting on brics"
+echo "Putin : just we are doing this its final to take down US"
+echo "Modi : Thats the plan ll workout my friend"
