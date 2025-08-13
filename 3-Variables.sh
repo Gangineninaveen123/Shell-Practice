0447ca09501bc00913b84952be167ee15ed4623b
Naveen G <gangineninaveen132@gmail.com>
2025-08-13 18:12:09 +0530

    conversation b/w Modi and Putin

--- 3-Variables.sh
+++ 3-Variables.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-Person1 = Modi
-Person2 = Putin
+PERSON1 = Modi
+PERSON2= Putin
 
 
-echo "$Person1 : Hello $Person2 Bhai, how are you"
-echo "$Person2 : I am super good, how are you my friend"
-echo "$Person1 : great my friend, lets make a meeting on brics"
-echo "$Person2 : just we are doing this its final to take down US"
-echo "$Person1 : Thats the plan ll workout my friend"
+echo "$PERSON1 : Hello $PERSON2 Bhai, how are you"
+echo "$PERSON2 : I am super good, how are you my friend"
+echo "$PERSON1 : great my friend, lets make a meeting on brics"
+echo "$PERSON2 : just we are doing this its final to take down US"
+echo "$PERSON1 : Thats the plan ll workout my friend"
