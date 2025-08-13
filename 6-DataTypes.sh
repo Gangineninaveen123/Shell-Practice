70efe17227bd47cd5575b453787dacdadadfcc8f
Naveen G <gangineninaveen132@gmail.com>
2025-08-14 00:39:48 +0530

    conversation b/w Modi and Putin

--- 6-DataTypes.sh
+++ 6-DataTypes.sh
@@ -3,3 +3,9 @@
 Timestamp=$(date) # dynamic output so we are pitting date in () braces
 
 echo "Date dynamic output : $Timestamp "
+
+num1=naveen
+num2=100
+
+sum=$(($num1+$num2))
+echo "sum of $num1 + $num2 is : $sum"
