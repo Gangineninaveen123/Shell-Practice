95946cee265803363726ad5e5ce5fe94456d90dc
Naveen G <gangineninaveen132@gmail.com>
2025-08-13 23:51:11 +0530

    conversation b/w Modi and Putin

---
+++ 4-arguments.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+
+echo "$PERSON1 : Hello $PERSON2 Bhai, how are you"
+echo "$PERSON2 : I am super good, how are you my friend"
+echo "$PERSON1 : great my friend, lets make a meeting on brics"
+echo "$PERSON2 : just we are doing this its final to take down US"
+echo "$PERSON1 : Thats the plan ll workout my friend"
