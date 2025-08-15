9c57779a4d8440657d389c62f99eeeac23ae1e44
Naveen G <gangineninaveen132@gmail.com>
2025-08-16 03:33:53 +0530

    Special Variables

--- 10-install-command.sh
+++ 10-install-command.sh
@@ -5,7 +5,7 @@ USERID=$( id -u)
 if [ $USERID -ne 0 ]
 then
     echo "ERROR:: Please run the shell script with root user"
-
+    exit 2 # give other than zero[1-127] as exit code, so it ll not move forward from this step.
 else
     echo "You are running with root user"
 
