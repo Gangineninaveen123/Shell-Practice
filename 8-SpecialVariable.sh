4fa7028827ced2939c2d701b0549bfc658415b31
Naveen G <gangineninaveen132@gmail.com>
2025-08-16 00:44:54 +0530

    Special Variables

--- 8-SpecialVariable.sh
+++ 8-SpecialVariable.sh
@@ -2,3 +2,9 @@
 
 echo "All variables passed to the shell script : $@ "
 echo "Number of variables passed : $# "
+echo "Current Directory : $PWD "
+echo " User running this script : $USER"
+echo " Home directory of the user : $HOME"
+echo " PID of the SCRIPT : $$ "
+sleep 10 &
+echo " PID last command in background : $! "
