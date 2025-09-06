0d3af5749c3e452e8ea8f91074b474d804fe5936
Naveen G <gangineninaveen132@gmail.com>
2025-09-07 00:20:18 +0530

    colours check

--- 12-colours.sh
+++ 12-colours.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-
-echo -e "\e[31m HelloWorld red colour"
+# \e is the starting, and we need to end, if not giving end, the downside also will have the same colour as same 
+echo -e "\e[31m HelloWorld red colour \e[0m"
 echo "Helloworld no colour"
