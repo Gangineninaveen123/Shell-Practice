88562fa7331c1d3a0966245694f40d7dbb664976
Naveen G <gangineninaveen132@gmail.com>
2025-09-14 12:54:51 +0530

    calling one cscript from another script

--- calling one script to another script_script1.sh
+++ calling one script to another script_script1.sh
@@ -2,5 +2,7 @@
 GREETINGS="HELLO I AM GOOD, HOW ARE YOU?"
 echo "$GREETINGS"
 echo "PID of script: $$"
-
-./script2.sh
+#method1 -> not recommonded
+# [./script2.sh] #here we can't access the content of script2 from script 1, so not to recommond
+#method 2 -> this one we can access everything from script2 from script one only ex: content
+source ./script2.sh 
