bfc4b7e5a0971e5147155428de594186984f8503
Naveen G <gangineninaveen132@gmail.com>
2025-09-19 17:11:40 +0530

    roboshop main common changes

--- 22-cpu-usage.sh
+++ 22-cpu-usage.sh
@@ -5,7 +5,9 @@
 while true;
 do
     CPU_USAGE=$(top -bn1 | grep "Cpu(s)" | awk '{print 100 - $8"%"}')
+    echo "CPU Utilization: $CPU_USAGE"
+    sleep 5
 done
 
 
-echo "CPU Utilization: $CPU_USAGE"
+
