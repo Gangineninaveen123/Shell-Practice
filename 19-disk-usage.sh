5c872617e86bdba7ca56e90e23c21057d9d6aea7
Naveen G <gangineninaveen132@gmail.com>
2025-09-19 10:56:16 +0530

    roboshop main common changes

---
+++ 19-disk-usage.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#Disk usage command, which removes headings like Use%
+DISK_USGAE=$(df -hT | grep -v Use%)
+
+#tHRESHOLD VALUE
+DISH_THRESHOLD=1 #In Project it ll be 75 
+
+# Reading line by line, which is executed in server, that one in done ll get, which those lines we are making itas code in do
+while IFS= read line
+do
+    #getting like below for usage command
+    # 0
+    # 0
+    # 2
+    # 42
+    USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1)
+
+    #Printing like /usr/bin [each location]
+    PARTITION=$(echo $line | awk -F " " '{print $7F}')
+
+    #Printing like ex: 0::/usr/bin [space in each location]
+    echo "$PARTITION:: $USAGE"
+
+done <<< $DISK_USGAE
